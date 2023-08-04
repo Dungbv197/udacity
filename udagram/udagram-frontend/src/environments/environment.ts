d024050ae07260b24faf6a79bd75679fa45aa566
DungBV <dungbv197@gmail.com>
2023-08-04 15:32:04 +0700

    run lint

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -1,11 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// The list of file replacements can be found in `angular.json`..
 
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagram-api-dev.us-west-2.elasticbeanstalk.com/api/v0'
 };
 
 /*
